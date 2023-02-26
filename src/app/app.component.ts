11763fd92f47f39c9c14457c0b9d562d770ceab1
tmjonker <tmjonker1@outlook.com>
2023-02-26 16:43:33 -0500

    Added shadow to name in jumbotron.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Timothy Jonker';
+  title = 'Timothy Jonker - Professional Full Stack Enginner';
 }
